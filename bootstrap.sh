b63bb4d62544f42595feb2e925349f4cc101d1e8
cfios4 <62921651+cfios4@users.noreply.github.com>
2023-07-12 22:05:07 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Set $USER variable
+read -p "Enter new local user's name: " RESU
+
 # Set UID 1000 to $RESU
-useradd -m -G wheel,docker -u 1000 casey
-RESU=$(getent passwd 1000 | cut -d':' -f1)
+useradd -m -G wheel,docker -u 1000 $RESU
+passwd -e $RESU
+
+# Baseline stuff
+dnf upgrade -y
+dnf install ansible -y
 
 # Install Docker and Tailscale
 curl -fsSL https://get.docker.com/ | bash
@@ -9,15 +16,8 @@ curl -fsSL https://tailscale.com/install.sh | bash
 systemctl enable docker --now
 systemctl enable tailscaled --now
 
-# Baseline stuff
-dnf upgrade -y
-dnf install ansible -y
-
-# Ansible pull repo
-ansible-pull -U https://github.com/cfios4/mediasvr-pull.git -d /home/$RESU
-
 # Create directories for app bind mounts
-mkdir -p /home/$RESU/swarmConfigs/apps/{caddy,flame,plex,radarr,sonarr,sabnzbd,vscode}
+mkdir -p /home/$RESU/swarmConfigs/apps/{caddy/{serve},flame,plex,radarr,sonarr,sabnzbd,vscode}
 
 # Test if $TS_KEY is defined
 if [ -z "$TS_API" ]; then
@@ -27,4 +27,7 @@ fi
 
 # Join Tailnet
 export TS_KEY=$(curl -s -H "Authorization: Bearer $TS_API" -d '{"capabilities":{"devices":{"create":{"reusable":false,"ephemeral":false,"preauthorized":true}}}}' https://api.tailscale.com/api/v2/tailnet/-/keys | grep -o '"key":"[^"]*"' | sed 's/"key":"\(.*\)"/\1/')
-tailscale up --auth-key=$TS_KEY --operator $RESU --ssh\
+tailscale up --auth-key=$TS_KEY --operator $RESU --ssh
+
+# Ansible pull repo
+ansible-pull -U https://github.com/cfios4/mediasvr-pull.git -d /home/$RESU
