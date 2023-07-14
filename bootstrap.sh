5bfba0a7b89ce61a328dbaaa374c008258948ada
casey@work <cfios@outlook.com>
2023-07-14 10:21:37 -0400

    Slight overhaul of script using IF statements

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# Baseline stuff
-dnf upgrade -y
-dnf install ansible git -y
+
+# Get OS
+OS=$(grep "^ID=" /etc/os-release | cut -d'=' -f2 | tr -d '"')
+
+if [ $OS = 'debian' ]; then
+    apt update -y
+    apt upgrade -y
+    apt install ansible git -y
+elif [ $OS = 'fedora' ]; then
+    dnf upgrade -y
+    dnf install ansible git -y
+elif [ $OS = 'alpine' ]; then
+    dnf upgrade -y
+    dnf install ansible git -y
+else
+    echo "Can't recognize operating system from /etc/os-release!"
+    exit
+fi
 
 # Install Docker and Tailscale
 curl -fsSL https://get.docker.com/ | bash
@@ -12,21 +27,21 @@ systemctl enable tailscaled --now
 # Set $RESU variable
 if [ -z "$RESU" ]; then
     # Variable is empty, ask for input
-    read -p "Enter new local user's name: " RESU
+    clear
+    read -p "Enter new local Docker user's name: " RESU
+# Test if $TS_API is defined
+elif [ -z "$TS_API" ]; then
+    # Variable is empty, ask for input
+    clear
+    read -p "Please enter Tailscale API key: " TS_API
 fi
 
 # Set UID 1000 to $RESU
 useradd -m -G wheel,docker -u 1000 $RESU
-passwd -e $RESU
+passwd -e $RESU > /dev/null
 
 # Create directories for app bind mounts
 mkdir -p /home/$RESU/swarmConfigs/appdata/{caddy/serve,flame,plex,radarr,sonarr,sabnzbd,vscode}
-
-# Test if $TS_KEY is defined
-if [ -z "$TS_API" ]; then
-    # Variable is empty, ask for input
-    read -p "Please enter Tailscale API key: " TS_API
-fi
 
 # Join Tailnet
 export TS_KEY=$(curl -s -H "Authorization: Bearer $TS_API" -d '{"capabilities":{"devices":{"create":{"reusable":false,"ephemeral":false,"preauthorized":true}}}}' https://api.tailscale.com/api/v2/tailnet/-/keys | grep -o '"key":"[^"]*"' | sed 's/"key":"\(.*\)"/\1/')
@@ -34,6 +49,9 @@ tailscale up --auth-key=$TS_KEY --operator $RESU --ssh
 
 # Set $TAILSCALEIP
 echo $(tailscale ip -4) >> /home/$RESU/.bashrc
+
+# Initialize Swarm
+docker swarm init --advertise-addr $TAILSCALEIP > /dev/null
 
 # Ansible pull repo
 ansible-pull -U https://github.com/cfios4/mediasvr-pull.git -d /home/$RESU
